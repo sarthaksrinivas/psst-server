1026291db326aee2841c5336fc51053eaf3a04fe
Sarthak Srinivas <sarthaksrinivas@gmail.com>
2018-01-28 03:36:28 -0500

    change mongodb uri

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/psst'
+    url: "mongodb://sarthaksrinivas:psst2017@ds117858.mlab.com:17858/heroku_nvj1r9t3"
+    // || 'mongodb://localhost:27017/psst'
 }
