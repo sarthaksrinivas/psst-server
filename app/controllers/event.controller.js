52a6b53a04bb7d555feee96b3ae7cc5632c4b907
Sarthak Srinivas <sarthaksrinivas@gmail.com>
2018-01-28 04:12:39 -0500

    local PUT working

--- app/controllers/event.controller.js
+++ app/controllers/event.controller.js
@@ -2,6 +2,10 @@ var Event = require('../models/event.model.js');
 
 exports.create = function (req, res) {
     // Create and Save a new Event
+
+    console.log("request: " + JSON.stringify(req.body));
+    console.log("response: " + JSON.stringify(res.params));
+
     var event = new Event({ title: req.body.title || "Untitled Event", description: req.body.description, lattitude: req.body.lattitude, longitude: req.body.longitude, votes: req.body.votes });
 
     event.save(function (err, data) {
@@ -17,6 +21,10 @@ exports.create = function (req, res) {
 
 exports.findAll = function (req, res) {
     // Retrieve and return all events from the database.
+
+    console.log("request: " + JSON.stringify(req.body));
+    console.log("response: " + JSON.stringify(res.params));
+
     Event.find(function (err, events) {
         if (err) {
             res.status(500).send({ message: "Some error ocuured while retrieving events." });
@@ -28,6 +36,10 @@ exports.findAll = function (req, res) {
 
 exports.findOne = function (req, res) {
     // Find a single event with a eventId
+
+    console.log("request: " + JSON.stringify(req.body));
+    console.log("response: " + JSON.stringify(res.params));
+
     Event.findById(req.params.eventId, function (err, data) {
         if (err) {
             res.status(500).send({ message: "Could not retrieve event with id " + req.params.eventId });
@@ -39,12 +51,16 @@ exports.findOne = function (req, res) {
 
 exports.update = function (req, res) {
     // Update a event identified by the eventId in the request
+
+    console.log("request: " + JSON.stringify(req.body));
+    console.log("response: " + JSON.stringify(res.params));
+
     Event.findById(req.params.eventId, function (err, event) {
         if (err) {
             res.status(500).send({ message: "Could not find a event with id " + req.params.eventId });
         }
 
-        event.title = req.body.title;
+        // event.title = req.body.title;
         event.content = req.body.content;
         event.votes = req.body.votes;
         event.lattitude = req.body.lattitude;
@@ -62,6 +78,10 @@ exports.update = function (req, res) {
 
 exports.delete = function (req, res) {
     // Delete a event with the specified eventId in the request
+
+    console.log("request: " + JSON.stringify(req.body));
+    console.log("response: " + JSON.stringify(res.params));
+
     Event.remove({ _id: req.params.eventId }, function (err, data) {
         if (err) {
             res.status(500).send({ message: "Could not delete event with id " + req.params.id });
@@ -74,6 +94,8 @@ exports.delete = function (req, res) {
 
 exports.coordinates = function (req, res) {
     // Get lattitude and longitude of an event
+    console.log(res.body.params);
+
     Event.findById(req.params.eventId, function (err, data) {
         if (err) {
             res.status(500).send({ message: "Could not retrieve event with id " + req.params.eventId });
@@ -85,6 +107,8 @@ exports.coordinates = function (req, res) {
 
 exports.votes = function (req, res) {
     // Get votes for an event
+    console.log(res.body.params);
+
     console.log("hi" + req.params.votes);
     Event.findById(req.params.eventId, function (err, data) {
         if (err) {
